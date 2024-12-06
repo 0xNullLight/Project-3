b6a830bb1540839c80f2332e3a742a34f36a7bc2
0xNullLight <37184669+0xNullLight@users.noreply.github.com>
2024-12-06 00:23:32 -0600

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Sample markdown files array (in a real application, this could be dynamic)
     const markdownFiles = [
-        { name: "Django Installation & Setting up a Project", url: "./markdown/Django_for_JavaScript_Developers.md" }
+        { name: "Django For JavaScript Developers", url: "./markdown/Django_for_JavaScript_Developers.md" }
     ];
     
     const dropdownEl = document.getElementById("file-dropdown");
