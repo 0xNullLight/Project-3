f68d34a09e121179ebf35a6113ec1f18d659a25d
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-12-07 15:06:23 -0600

    added the connection to models file from admin

--- Project3Quiz/Project3Quiz/Admin.py
+++ Project3Quiz/Project3Quiz/Admin.py
@@ -1,2 +1,17 @@
 # This file will connect the models to the admin panel
 
+from django.contrib import admin
+from .Models import Student, Question, QuestionOption
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['username', 'score', 'status']
+    
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question', 'option1', 'option2', 'option3', 'option4', 'answer']
+    
+@admin.register(QuestionOption)
+class QuestionOptionAdmin(admin.ModelAdmin):
+    list_display = ['question', 'option1', 'option2', 'option3', 'option4', 'is_correct']
+    
