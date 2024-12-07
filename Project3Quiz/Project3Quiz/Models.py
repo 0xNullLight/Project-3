b10b3ec49bfe7c9e6110fd922ff712432f7cd20b
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-12-07 14:54:39 -0600

    added function

--- Project3Quiz/Project3Quiz/Models.py
+++ Project3Quiz/Project3Quiz/Models.py
@@ -3,35 +3,35 @@ from django.db import models
 
 # This class defines the Student model, username, score, and status fields
 class Student(models.Model):
-    username = models.CharField(max_length=100)
-    score = models.IntegerField()
-    status = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
+    score = models.IntegerField(default=0)
+    status = models.CharField(max_length=100, default="active")
 def __str__(self):
     return self.username
 
 # Class for Question model, question, option1, option2, option3, option4, and answer fields
     class Question(models.Model):
-        question = models.CharField(max_length=1000)
+        question = models.CharField(max_length=1000, unique=True)
         def __str__(self):
             return self.question
         
-        option1 = models.CharField(max_length=1000) 
+        option1 = models.CharField(max_length=1000, unique=True) 
         def __str__(self):
             return self.option1
         
-        option2 = models.CharField(max_length=1000)
+        option2 = models.CharField(max_length=1000, unique=True)
         def __str__(self):
             return self.option2
         
-        option3 = models.CharField(max_length=1000)
+        option3 = models.CharField(max_length=1000, unique=True)
         def __str__(self):
             return self.option3
         
-        option4 = models.CharField(max_length=1000)
+        option4 = models.CharField(max_length=1000, unique=True)
         def __str__(self):
             return self.option4
         
-        answer = models.CharField(max_length=1000)
+        answer = models.CharField(max_length=1000, unique=True)
         def __str__(self):
             return self.answer
         
