a29ffa076686ddf7727bec134c75f6b670f55229
0xNullLight <37184669+0xNullLight@users.noreply.github.com>
2024-12-07 09:32:48 -0600

    Update notes.js

--- notes.js
+++ notes.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     // Sample markdown files array (in a real application, this could be dynamic)
     const markdownFiles = [
         { name: "Django For JavaScript Developers", url: "./markdown/Django_for_JavaScript_Developers.md" },
-        { name: "Installing Virtual Environment for Python in Visual Studio to Work on Django", url: "./markdown/Installing_Virtual_Environment_for_Python_in_Visual_Studio_to_Work_on_Django.md" }
+        { name: "Installing Virtual Environment for Python in Visual Studio to Work on Django (Windows, macOS, Linux)", url: "./markdown/Installing_Virtual_Environment_for_Python_in_Visual_Studio_to_Work_on_Django.md" },
+        { name: "Navigating Python with Visual Studio and Jupyter Lab Extension: The Essential Functions You Won't Find in Textbooks", url: "./markdown/Navigating_Python_&_Jupyter_Lab_Extension:The_Essential_Functions_You_Won't_Find_in_Textbooks.md }
     ];
     
     const dropdownEl = document.getElementById("file-dropdown");
