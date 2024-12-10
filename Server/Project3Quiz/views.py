c13b1d014164185164a97115d9fe8ba4d55b5c6a
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-12-10 10:21:47 -0600

    added

--- Server/Project3Quiz/views.py
+++ Server/Project3Quiz/views.py
@@ -1,2 +1,9 @@
-# Api Endpoints will go here
+# Api Endpoints will go here 
+# We will Create a function for each endpoint
+
+from django.shortcuts import render
+from .models import Question, Student
+from .Serializers import QuestionSerializer
+from rest_framework.response import UserResponse 
+from rest_framework.decorators import api_views
 
