f35f0f53ca361d0402a14f5c64b90d37272c5c20
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-12-10 11:45:23 -0600

    merged the template branch to the main

--- Server/Project3Quiz/views.py
+++
@@ -1,9 +0,0 @@
-# Api Endpoints will go here 
-# We will Create a function for each endpoint
-
-from django.shortcuts import render
-from .models import Question, Student
-from .Serializers import QuestionSerializer
-from rest_framework.response import UserResponse 
-from rest_framework.decorators import api_views
-
