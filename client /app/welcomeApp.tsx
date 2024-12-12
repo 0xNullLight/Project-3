1ae43180eff80c7a1c15cca54d6e5b873016c88f
amanda forrester <a.soriano92@aol.com>
2024-12-12 13:45:46 -0600

    Update welcomeApp.tsx

--- client /app/welcomeApp.tsx
+++ client /app/welcomeApp.tsx
@@ -1,16 +1,1 @@
-import WelcomeScreen from './WelcomeScreen';
 
-const App = () => {
-  return (
-    <WelcomeScreen
-      logoSrc="/path/to/logo.png"
-      headingText="Welcome to our platform"
-      primaryButtonText="Get Started"
-      secondaryButtonText="Menu"
-      onPrimaryClick={() => console.log('Primary button clicked')}
-      onSecondaryClick={() => console.log('Secondary button clicked')}
-    />
-  );
-};
-
-export default App;
