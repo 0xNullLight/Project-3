ce4fc7ed0ace3a08201b47eb899c9475fa03a8d5
Claree90 <claree8@gmail.com>
2024-12-12 14:12:16 -0600

    Create LoginPage.style.ts

---
+++ client/app /LoginPage.style.ts
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+
+export const PageWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background: #323842;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Container = styled.div`
+  width: 700px;
+  padding: 40px;
+  background: #fff;
+  border-radius: 8px;
+  box-shadow: 0px 0px 1px rgba(23, 26, 31, 0.07),
+              0px 0px 2px rgba(23, 26, 31, 0.12);
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const TextBox = styled.input`
+  width: 100%;
+  height: 60px;
+  padding: 0 20px;
+  font-family: Inter, sans-serif;
+  font-size: 34px;
+  line-height: 52px;
+  background: #fff;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  outline: none;
+
+  &:hover, &:focus {
+    color: #5C5E70;
+    border-color: #6C6794;
+  }
+
+  &:disabled {
+    color: #5C5E70;
+    opacity: 0.4;
+  }
+`;
+
+export const LoginButton = styled.button`
+  width: 100%;
+  height: 92px;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: Inter, sans-serif;
+  font-size: 34px;
+  line-height: 52px;
+  font-weight: 400;
+  color: #fff;
+  background: #6C6794;
+  border: none;
+  border-radius: 2px;
+  cursor: pointer;
+
+  &:hover {
+    background: #5d578f;
+  }
+
+  &:active {
+    background: #4e4977;
+  }
+
+  &:disabled {
+    opacity: 0.4;
+    cursor: not-allowed;
+  }
+`;
