fd9f235f80f001890270d44a503486c498a74885
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-12-13 12:00:13 -0600

    Update Index.ts

--- client/app /Index.ts
+++ client/app /Index.ts
@@ -1,1 +1,5 @@
+/* The code's purpose is to take a component called "LastScreen" from a local file 
+and make it accessible to other files that want to use it. It acts like a doorway 
+that allows other parts of the program to access this specific component.*/
+
 export { default as LastScreen } from './LastScreen';
